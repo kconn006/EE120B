fe092c354c329c4dc4d9d142c3dfae0189b92c81
kconn006 <kconn006@ucr.edu>
2018-07-31 14:29:25 -0700

    main.c

--- main.c
+++ main.c
@@ -3,39 +3,37 @@
  *
  * Created: 7/31/2018 9:38:48 AM
  * Author : Keilani
+ *Partner : Kiana McCullough
  */ 
-
 #include <avr/io.h>
-#define garage_sensor PINA 
-#define light_sensor PINA 
-#define  LED PORTA 
+
 int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-	
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs,
+
+	// initialize to 0s
+	unsigned char tmpB = 0x00; 	// Temporary variable to hold the value of B
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+
 	while(1)
 	{
-		// 1) Read input
-		//light sensor = PA1
-		light_sensor = PINA & 0x03;
-		garage_sensor = PINA & 0x01;
+		// (1) Read input
+		tmpA = PINA & 0x03;
+		
+		//led = 1 when door is open and light is 0
 
-		// 2) Perform computation
-		// if PA1 is 1, set PB0 = 00, else = if PA0 is 1, PB0 = 1
-		if (light_sensor & 0x01 == 0) { 
-			LED = 0;
+		if (tmpA == 0x01) { // True if PA0 is 1
+			tmpB = (tmpB & 0xFE) | 0x01;
+			// (clear rightmost 2 bits, then set to 1)
 		}
+
 		else {
-			//Garage_sensor = PA0
-			if (garage_sensor == 1) {
-				LED = 1;
-			}
-			if else {
-				LED = 0;
+			tmpB = 0x00;
 		}
-		// 3) Write output
-		PORTB = LED;
+
+		PORTB = tmpB;
 	}
+	
 	return 0;
 }
