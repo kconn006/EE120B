f8e8cd504a03436709c7583aa56703ad2dd17c64
kconn006 <kconn006@ucr.edu>
2018-07-31 21:08:21 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,39 +1,50 @@
 /*
- * EE120B_lab1_pt1.c
+ * EE120B_lab1_part3.c
  *
- * Created: 7/31/2018 9:38:48 AM
+ * Created: 7/31/2018 9:05:15 PM
  * Author : Keilani
  *Partner : Kiana McCullough
  */ 
+*/
+
 #include <avr/io.h>
+
 
 int main(void)
 {
-	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs,
-
-	// initialize to 0s
-	unsigned char tmpB = 0x00; 	// Temporary variable to hold the value of B
-	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
-
-	while(1)
-	{
-		// (1) Read input
-		tmpA = PINA & 0x03;
-		
-		//led = 1 when door is open and light is 0
-
-		if (tmpA == 0x01) { // True if PA0 is 1
-			tmpB = (tmpB & 0xFE) | 0x01;
-			// (clear rightmost 2 bits, then set to 1)
-		}
-
-		else {
-			tmpB = 0x00;
-		}
-
-		PORTB = tmpB;
-	}
+	/* Replace with your application code */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	unsigned char cntavail = 0;
 	
+	while (1)
+	{
+		cntavail = 0;
+		
+		if(PINA & 0x01) { //bit 0
+			cntavail += 1;
+		}
+		
+		if(PINA & 0x02) { //bit 1
+			cntavail += 1;
+		}
+		
+		if(PINA & 0x04) { //bit 2
+			cntavail += 1;
+		}
+		
+		if(PINA & 0x08) { //bit 3
+			cntavail += 1;
+		}
+		
+		PORTC = 4-cntavail;
+		
+		if (PORTC == 0x00) { //if full
+			PORTC = (PORTC & 0xFF) | 0x80; //set bit 7 to 1
+		}
+	}
+
 	return 0;
 }
+/*Extend the previous program to still write the available spaces number,
+but only to PC3..PC0, and to set PC7 to 1 if the lot is full.*/
